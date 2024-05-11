acb9e3a63ddf336f5ab58823a7440fbd4226346b
nguyendat13 <dat48421@gmail.com>
2024-05-11 20:15:03 +0700

    buoi4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Logo from "./assets/images/logo.webp";
+import "./layoutStyle.css";
+import { IoBagHandle } from "react-icons/io5";
 function App() {
+  const hoten= "Nguyen Phat Dat";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <header className="header">
+    <div className="container">
+      <div className="row">
+      <div className="col-md-2">
+        <img className="hinh" src={Logo} alt="logo"/>
+      </div>
+        <div className="col-md-6"> 
+        <nav className="nav nav-pills flex-column flex-sm-row ">
+  <a className=" nav-link" href="#">Active</a>
+  <a className=" nav-link" href="#">Longer nav link</a>
+  <a className=" nav-link" href="#">Link</a>
+  <a className=" nav-link " >Disabled</a>
+  </nav></div>
+        <div className="col-md-2">
+             <div class="search-container">
+              <input type="text" id="searchInput" placeholder="Tìm kiếm sản phẩm"/>
+              </div>
+              <IoBagHandle />
+        </div>
+      </div>
     </div>
+    </header>
+    <main></main>
+    <footer></footer>
+    </>
   );
 }
 
